6738efd648e531c1c87a5e54a6ccbb3bcdd53207
Jeffrey Mahmood <jmahmood@cubesmart.com>
2023-04-26 20:28:39 -0400

    30: validate tweets and schedule first tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,11 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: { minimum: 1, maximum: 280, too_long: "A tweet is only 280 max. Everybody knows that!" }
+  validates :publish_at, presence: true
+
+  after_initialize do
+    self.publish_at ||= 24.hours.from_now
+  end
 end
